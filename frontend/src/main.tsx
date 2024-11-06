6ad2dbfb6825df78a9bba33fed18a9b060d95ee5
harrykamboj1 <singhharnoor116@gmail.com>
2024-11-06 23:40:25 +0530

    Landing Page and Dashboard Page added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./auth/sign-in/SignInPage";
+import ReactDOM from "react-dom/client";
+import React from "react";
 import SignUpPage from "./auth/sign-up/SignUpPage";
+import App from "./App";
+import { LandingPage } from "./Home/LandingPage";
+import Dashboard from "./dashboard/dashboard";
 
 const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
   {
     path: "/auth/sign-in",
     element: <SignInPage />,
@@ -16,8 +31,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </React.StrictMode>
 );
